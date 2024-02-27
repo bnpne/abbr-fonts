5314a917c22c51cd527fe0555887484d99f0066b
Ben Paine <benpaine@Bens-MBP.lan>
2024-02-27 01:35:08 -0800

    fix

--- app/api/revalidate/route.js
+++ app/api/revalidate/route.js
@@ -1,6 +1,6 @@
 import {revalidateTag} from 'next/cache'
 
-export async function POST(_request) {
+export async function POST() {
   revalidateTag('graphql')
   return NextResponse.json({revalidated: true, now: Date.now()})
 }
