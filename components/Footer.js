79bb1bb102bf213fae786a0b07abb71ed625d44d
Ben Paine <bentppaine@gmail.com>
2024-03-11 20:51:17 -0700

    fixes

--- components/Footer.js
+++ components/Footer.js
@@ -16,16 +16,16 @@ export default function Footer({viewer}) {
 
   useGSAP(
     () => {
-      const st = gsap.utils.toArray('[data-text-inner]', text.current)
-      const ar = Array.prototype.slice.call(grid.current.children)
-      gsap
-        .timeline({
-          scrollTrigger: {
-            trigger: footer.current,
-            start: 'top 90%',
-          },
-        })
-        .fromTo(
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: footer.current,
+          start: 'top 90%',
+        },
+      })
+      if (path !== '/info') {
+        const st = gsap.utils.toArray('[data-text-inner]', text.current)
+        const ar = Array.prototype.slice.call(grid.current.children)
+        tl.fromTo(
           ar,
           {
             y: '100%',
@@ -36,89 +36,112 @@ export default function Footer({viewer}) {
             ease: 'expo.out',
             duration: 1,
           },
-        )
-        .from(
+        ).from(
           st,
           {
             y: '100%',
+            opacity: 0,
             stagger: 0.05,
             ease: 'expo.out',
             duration: 0.6,
           },
           '<40%',
         )
-        .from(
-          pill.current,
-          {
-            bottom: -50,
-            ease: 'expo.out',
-            duration: 0.5,
-          },
-          '<50%',
-        )
+      }
+      tl.from(
+        pill.current,
+        {
+          bottom: -50,
+          ease: 'expo.out',
+          duration: 0.5,
+        },
+        '<50%',
+      )
     },
     {dependencies: [path]},
   )
 
   return (
     <footer ref={footer} className="footer">
-      <div className="footer-container">
-        <div className="footer-text">
-          <h2 ref={text}>
-            <span className="footer-text-outer">
-              <span data-text-inner className="footer-text-inner">
-                AbbrEVIATED Foundry is an ongoing
+      {path !== '/info' && (
+        <div className="footer-container">
+          <div className="footer-text">
+            <h2 ref={text}>
+              <span className="footer-text-outer">
+                <span data-text-inner className="footer-text-inner">
+                  Abbreviated Foundry is an ongoing study in typography by
+                </span>
               </span>
-            </span>
-            <span className="footer-text-outer">
-              <span data-text-inner className="footer-text-inner">
-                STUDY in typography by AbbrEVIATED
+              <span className="footer-text-outer">
+                <span data-text-inner className="footer-text-inner">
+                  Abbreviated Projects. The fonts on this site are Entirely
+                </span>
               </span>
-            </span>
-            <span className="footer-text-outer">
-              <span data-text-inner className="footer-text-inner">
-                Projects. The FONTS on this site are
+              <span className="footer-text-outer">
+                <span data-text-inner className="footer-text-inner">
+                  designed by and sold exclusively on Abbr. Foundry.
+                </span>
               </span>
-            </span>
-            <span className="footer-text-outer">
-              <span data-text-inner className="footer-text-inner">
-                Entirely designed BY AND SOLD
-              </span>
-            </span>
-            <span className="footer-text-outer">
-              <span data-text-inner className="footer-text-inner">
-                EXCLUSIVELY ON AbbrEVIATED foundry.
-              </span>
-            </span>
-          </h2>
-        </div>
-        <div ref={grid} className="footer-grid">
-          <div className="footer-grid-col"></div>
-          <div className="footer-grid-col"></div>
-          <div className="footer-grid-col"></div>
-          <div className="footer-grid-col"></div>
-        </div>
-        <div ref={pill} className="footer-pill">
-          <div className="footer-links">
-            <FontdueHTML
-              className="footer-link"
-              html={viewer.settings?.footerText}
-            />
-            <Link
-              className="footer-link"
-              href="https://www.instagram.com/abbr.projects/"
-            >
-              Instagram
-            </Link>
-            <Link className="footer-link" href="mailto:simon@abranowicz.com">
-              Contact
-            </Link>
-            <Link className="footer-link" href="https://www.abbrprojects.com/">
-              ABBR. Projects
-            </Link>
+            </h2>
+          </div>
+          <div ref={grid} className="footer-grid">
+            <div className="footer-grid-col"></div>
+            <div className="footer-grid-col"></div>
+            <div className="footer-grid-col"></div>
+            <div className="footer-grid-col"></div>
+          </div>
+          <div ref={pill} className="footer-pill">
+            <div className="footer-links">
+              <FontdueHTML
+                className="footer-link"
+                html={viewer.settings?.footerText}
+              />
+              <Link
+                className="footer-link"
+                href="https://www.instagram.com/abbr.projects/"
+              >
+                Instagram
+              </Link>
+              <Link className="footer-link" href="mailto:simon@abranowicz.com">
+                Contact
+              </Link>
+              <Link
+                className="footer-link"
+                href="https://www.abbrprojects.com/"
+              >
+                ABBR. Projects
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
+      )}
+      {path === '/info' && (
+        <div className="footer-info-container">
+          <div ref={pill} className="footer-pill">
+            <div className="footer-links">
+              <FontdueHTML
+                className="footer-link"
+                html={viewer.settings?.footerText}
+              />
+              <Link
+                className="footer-link"
+                href="https://www.instagram.com/abbr.projects/"
+              >
+                Instagram
+              </Link>
+              <Link className="footer-link" href="mailto:simon@abranowicz.com">
+                Contact
+              </Link>
+              <Link
+                className="footer-link"
+                href="https://www.abbrprojects.com/"
+              >
+                ABBR. Projects
+              </Link>
+            </div>
+          </div>
+        </div>
+      )}
     </footer>
   )
 }
