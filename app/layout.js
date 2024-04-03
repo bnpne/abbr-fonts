19aecbad6dddf1b17062fe564a8ef500e784ed6f
Ben Paine <bentppaine@gmail.com>
2024-04-03 10:09:40 -0700

    remove bespoke from cart

--- app/layout.js
+++ app/layout.js
@@ -59,6 +59,7 @@ export default async function RootLayout({children}) {
         <FontdueProvider
           config={{
             typeTester: {selectable: true, variableAxesPosition: 'auto'},
+            storeModal: {indexExcludeTags: ['Bespoke']},
           }}
         >
           <Layout>
